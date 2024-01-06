9d80590508c6fc302749d544a5b51617498c1707
Yusuf Duman <yusufdumans61@gmail.com>
2024-01-06 03:40:17 +0300

    Create Order & Create Furniture pages added.

---
+++ Web/Pages/CreateOrder.cshtml.cs
@@ -0,0 +1,16 @@
+using Web.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Web.Entities;
+
+namespace Web.Pages
+{
+    public class CreateOrderModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
